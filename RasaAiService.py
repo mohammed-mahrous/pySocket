d099330c2008956f443922061060b3ef29fb20d0
mohamed-mahrous <xxspider6@gmail.com>
2024-06-02 21:14:25 +0300

    first commit

---
+++ RasaAiService.py
@@ -0,0 +1,126 @@
+import requests
+HOST = 'localhost'
+PORT = 5005
+TOKEN = None
+CON_ID = 7
+
+
+class RasaAiService:
+    def __init__(self, host:str=HOST, port:int=PORT, authToken:str=TOKEN, Conversation_Id:int=CON_ID) -> None:
+        self.host = host
+        self.port = port
+        self.authToken = authToken
+        self.Conversation_Id =  Conversation_Id
+        self.__baseUrl = f'http://{self.host}:{self.port}'
+        self.__currentIntent = {}
+        self.__allMessages:list[str] = []
+        self.__currentMessage:str = None
+
+    def checkServerStatus(self) -> bool:
+        endPoint = '/status'
+        try:
+            res = requests.get(self.__baseUrl+endPoint,params={'token': self.authToken})
+            if not res.status_code == 200:
+                return False
+            return True
+        except Exception as ex:
+            print('exception occured: {ex}'.format(ex))
+            return False
+    
+    def sendMessage(self,message:str) -> None:
+        endpoint = f'/conversations/{self.Conversation_Id}/messages'
+        body = {
+        'text':message,
+        'sender': 'user'
+        }
+        try:
+            res = requests.post(self.__baseUrl+endpoint,json=body)
+            resJson = res.json()
+            resMessage = resJson['latest_message']
+            intent:dict = resMessage['intent']
+            self.__currentIntent = intent
+        except Exception as ex:
+            print(f'exception occured: {ex}')
+
+    def __getIntentName(self):
+        return self.__currentIntent['name'] if self.__currentIntent != None else None
+    
+
+    def triggerIntent(self) -> None:
+        endPoint = f'/conversations/{self.Conversation_Id}/trigger_intent'
+        try:
+            if(self.__currentIntent == None):
+                raise Exception(message='no intent was found')
+            body = {
+            "name": self.__getIntentName()
+            }
+            res = requests.post(self.__baseUrl+endPoint,json=body)
+            resjson = res.json()
+            self.__currentIntent = None
+            if not len(resjson['messages']) > 0 :
+                self.__currentMessage = None
+                return
+            messages:list[dict] = resjson['messages']
+            text = ''
+            if(not len(messages) > 1):
+                text = messages[0]['text']
+                self.__currentMessage = text;
+                self.__allMessages.append(text)
+                return
+            for message in messages:
+                keys = message.keys()
+                for key in keys:
+                    if key == 'text':
+                        text = text + f'{message[key]} \n'
+                    if(key == 'image'):
+                        pass
+                        # text = text + f'صورة : {message[key]} \n'
+            self.__currentMessage = text;
+            self.__allMessages.append(text)
+            return
+        except Exception as ex:
+            print(f'exception occured: {ex}')
+
+    
+    def getApiResponseFromMessageAsText(self, message:str) -> str:
+        self.sendMessage(message)
+        self.triggerIntent()
+        return self.__currentMessage
+    
+
+    # def __getAudioBytesFromMessage(self):
+    #     try:
+    #         data = self.ttsService.text_to_speech(self.__currentMessage)
+    #         return data
+    #     except Exception as ex:
+    #         print(f'exception occured: {ex}')
+    
+    # def getApiResponseFromMessageAsSound(self, message:str) -> None:
+    #     try:    
+    #         if(not self.checkServerStatus()):
+    #             raise Exception(message='server status is offline')
+    #         self.sendMessage(message)
+    #         self.triggerIntent()
+    #         if(self.__currentMessage == None):
+    #             return
+    #         if(len(self.__currentMessage) > 0):
+    #             self.__getAudioFromMessage()
+    #         else: print('no message was found')
+    #     except Exception as exp:
+    #         print(f'exception occured: {exp}')
+    #         print('closing program')
+    #         exit()
+
+    # def getApiResponseFromMessageAsbytes(self, message:str) -> bytes:
+    #     try:    
+    #         if(not self.checkServerStatus()):
+    #             raise Exception(message='server status is offline')
+    #         self.sendMessage(message)
+    #         self.triggerIntent()
+    #         if(self.__currentMessage == None):
+    #             return
+    #         if(len(self.__currentMessage) > 0):
+    #            return self.__getAudioBytesFromMessage()
+    #         else: print('no message was found')
+    #     except Exception as exp:
+    #         print(f'exception occured: {exp}')
---
+++ server.py
@@ -0,0 +1,102 @@
+import socket
+from RasaAiService import RasaAiService
+import requests
+import time,os
+from pydub import AudioSegment , playback
+import pyaudio
+from typing import IO , Any
+
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+print("format: {}".format(FORMAT))
+class Server :
+    def __init__(self, host:str, port:int) -> None:
+        self.host = host
+        self.port = port
+        self.sock = socket.socket()
+        self.serving = False
+        self.aiService = RasaAiService()
+
+    def serve(self):
+        self.sock.bind((self.host,self.port))
+        self.serving = True
+        self.sock.listen()
+        self.__handleConnection()
+        self.serving = False
+
+
+    def _handleAudioBytes(self, data:bytes, id:str) -> requests.Response:
+        # pass
+        dataLen = len(data)
+        sample_width = 2 if dataLen % (CHANNELS * 2) == 0 else 4 if dataLen % (CHANNELS * 4) == 0 else 1
+        audio = AudioSegment(data,channels=CHANNELS, frame_rate=RATE,sample_width=sample_width)
+        exportData = audio.export(out_f='temp-audio-{}.wav'.format(id.replace('.','')),format='wav')
+        with open(exportData.name,'rb') as wav_file:
+                res = requests.post('http://localhost:5000/transcript', files={'wav-file': wav_file.read()})
+        exportData.close()
+        os.remove(exportData.name)
+        return res
+
+    def __handleConnection(self):
+        conn , address = self.sock.accept()
+        print("Connection from: " + str(address))
+        while True:
+            # receive data stream. it won't accept data packet greater than 1024 bytes
+            data = conn.recv(100000 * 2)
+            if not data:
+                break
+            # res = self.aiService.getApiResponseFromMessageAsText(str(data))
+            res = self._handleAudioBytes(data,address[0])
+            # with open(exportedData.name,'rb') as wav_file:
+            #     res = requests.post('http://localhost:5000/transcript', files={'wav-file': wav_file.read()})
+            
+            # exportedData.close()
+            # os.remove(exportedData.name)
+
+            if(res.status_code != 200):
+                break
+            transcript:str = res.json()['transcript']
+
+            if(len(transcript.strip()) != 0):
+                ai_response = self.aiService.getApiResponseFromMessageAsText(transcript.strip())
+                print("ai response: {}".format(ai_response))
+
+            # start = time.time()
+            # import whisper
+            # model = whisper.load_model('tiny', device='cpu')
+            # results = model.transcribe(audio=exportData.name,language='ar')
+            # print('results: {}'.format(results['text']))
+            # end = time.time()
+            # print(f"Time taken to run the code by function was {end-start} seconds")
+            
+
+
+            # p = pyaudio.PyAudio()
+            # stream = p.open(format=FORMAT,
+            #         channels=CHANNELS,
+            #         rate=RATE,
+            #         output=True,)
+            # stream.write(audio)
+            # with tempfile.NamedTemporaryFile(mode='bx',suffix='.wav',delete=False) as f:
+            #     f.write(data)
+            #     fileName = f.name
+            
+            #     with open(fileName,'rb') as rf:
+            #         # print('file-bytes-read: {}'.format(rf.read()))
+            #         res = requests.post('http://localhost:5000/transcript', files={'wav-file': rf.read()});
+            
+            # res_json = res.json()
+            # print('response_json: {}'.format(res_json))
+            # res_bytes = base64.b64decode(res_data['bytes'])
+            # conn.send(data_bytes)
+        conn.close() # close the connection
+        # self.sock.close()
+
+
+if __name__ == "__main__":
+    HOST,PORT = 'localhost', 9999
+    server = Server(HOST,PORT)
+    server.serve()
